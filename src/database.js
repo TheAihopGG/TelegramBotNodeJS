745be7ccc9fcfb845214e859f04dc31ee9d19cb4
TheAihopGG <108916224+TheAihopGG@users.noreply.github.com>
2025-01-02 14:57:24 +0700

    feat: tables creation: users, admins, products, orders, tickets

---
+++ src/database.js
@@ -0,0 +1,36 @@
+function createTables(db) {
+    db.exec(`
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY,
+            userName TEXT,
+            banned INTEGER DEFAULT 0,
+            banReason TEXT DEFAULT ""
+        );
+        CREATE TABLE IF NOT EXISTS admins (
+            id INTEGER PRIMARY KEY,
+            userName TEXT
+        );
+        CREATE TABLE IF NOT EXISTS products (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            description TEXT DEFAULT "",
+            image_urls JSON DEFAULT [],
+            cost INTEGER,
+            inStock INTEGER DEFAULT 1
+        );
+        CREATE TABLE IF NOT EXISTS orders (
+            id INTEGER PRIMARY KEY,
+            productId INTEGER,
+            userId INTEGER,
+            commentary TEXT DEFAULT "",
+            done INTEGER DEFAULT 0
+        );
+        CREATE TABLE IF NOT EXISTS tickets (
+            id INTEGER PRIMARY KEY,
+            userId INTEGER,
+            commentary TEXT DEFAULT "",
+            image_urls JSON DEFAULT [],
+            closed INTEGER DEFAULT 0
+        );
+    `);
+}
